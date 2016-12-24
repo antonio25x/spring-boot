281b0114614ee31e4aa7b5281246556719a64511
Programador Inca <programador.inca@gmail.com>
2016-12-23 23:07:58 -0500

    Add missing SpringBootApplication annotation

--- src/main/java/com/springboot/main/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/main/SpringBootDemoApplication.java
@@ -2,15 +2,16 @@ package com.springboot.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@SpringBootApplication
 @Configuration
 @ComponentScan({"com.springboot.controller", "com.springboot.main"})
 @EnableAutoConfiguration
+@SpringBootApplication
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
