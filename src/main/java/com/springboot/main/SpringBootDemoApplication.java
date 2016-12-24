5c7ee26f77154d8b09f4f0e938f87ea5c91a403a
Programador Inca <programador.inca@gmail.com>
2016-12-23 22:39:15 -0500

    Add backbone code for Spring boot

---
+++ src/main/java/com/springboot/main/SpringBootDemoApplication.java
@@ -0,0 +1,24 @@
+package com.springboot.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@SpringBootApplication
+@Configuration
+@ComponentScan({"com.springboot.controller", "com.springboot.main"})
+@EnableAutoConfiguration
+public class SpringBootDemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootDemoApplication.class);
+	}
+}
