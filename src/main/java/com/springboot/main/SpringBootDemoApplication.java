6a05b0e4674c09032252ce14fde7d7a435d9d586
Programador Inca <programador.inca@gmail.com>
2016-12-28 20:04:56 -0500

    Remove component scan for main package

--- src/main/java/com/springboot/main/SpringBootDemoApplication.java
+++ src/main/java/com/springboot/main/SpringBootDemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"com.springboot.controller", "com.springboot.main"})
+@ComponentScan({"com.springboot.controller"})
 @EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
